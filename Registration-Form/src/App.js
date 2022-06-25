1ecbe7cb97cea9397d02e8d6122be4223ab1e523
Riya Singhal <105406278+SinGHAl-Riya@users.noreply.github.com>
2022-06-25 11:27:10 +0530

    Add files via upload

---
+++ Registration-Form/src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+import Forminputs from './newComponent/newComponent';
+
+function App() {
+  return (
+      <div>
+        <Forminputs/>
+      </div>
+  
+  );
+}
+
+export default App;
